e2f27e92b34bccf9760138d9ef770fa638d2aa87
PandyYang <yangpandy@gmail.com>
2021-05-16 11:57:01 +0800

    修复登录验证问题

--- src/main/java/com/pandy/blog/auth/MyUserDetailsService.java
+++ src/main/java/com/pandy/blog/auth/MyUserDetailsService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,34 +29,57 @@ public class MyUserDetailsService  implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        if (username == null) {
+            throw new UsernameNotFoundException("用户名不能为空");
+        }
+
         User user = userService.getUserByName(username);
         if (user == null) {
             throw new UsernameNotFoundException("用户不存在");
         }
 
-        //用户角色
+        List<GrantedAuthority> authorities = new ArrayList<>();
         List<Role> roles =  user.getRoles();
-        if (roles.size() > 0) {
-            MyUserDetails myUserDetails = new MyUserDetails();
-            myUserDetails.setUser(user);
-//        myUserDetails.setUsername("admin");
-//        myUserDetails.setPassword("{noop}123456");
-            myUserDetails.setUsername(user.getUsername());
-            myUserDetails.setPassword("{noop}" + user.getPassword()); // noop表示未加密状态
+//        List<String> roleNames = null;
+//        for (Role role : roles) {
+//            String res = role.getRoleName();
+//            roleNames.add(res);
+//        }
 
-            SimpleGrantedAuthority authority;
-            Set<GrantedAuthority> authorities = new HashSet<>();
-            for (Role role : roles) {
-                authority = new SimpleGrantedAuthority(role.getRoleName());
-                authorities.add(authority);
-            }
-            myUserDetails.setAuthorities(authorities);
-            UserAuth userAuth=new UserAuth();
-            userAuth.setUser(user);
-            userAuth.setAuthorities(authorities);
-            return  userAuth;
-        } else {
-            throw  new UsernameNotFoundException("没有该用户");
+        SimpleGrantedAuthority simpleGrantedAuthority = null;
+        for (Role role : roles) {
+            simpleGrantedAuthority = new SimpleGrantedAuthority(role.getRoleName());
+            authorities.add(simpleGrantedAuthority);
         }
+
+        UserAuth userAuth = new UserAuth();
+        userAuth.setUser(user);
+        userAuth.setAuthorities(authorities);
+        return userAuth;
+
+
+//        //用户角色
+//        if (roles.size() > 0) {
+//            MyUserDetails myUserDetails = new MyUserDetails();
+//            myUserDetails.setUser(user);
+////        myUserDetails.setUsername("admin");
+////        myUserDetails.setPassword("{noop}123456");
+//            myUserDetails.setUsername(user.getUsername());
+//            myUserDetails.setPassword("{noop}" + user.getPassword()); // noop表示未加密状态
+//
+//            SimpleGrantedAuthority authority;
+//            Set<GrantedAuthority> authorities = new HashSet<>();
+//            for (Role role : roles) {
+//                authority = new SimpleGrantedAuthority(role.getRoleName());
+//                authorities.add(authority);
+//            }
+//            myUserDetails.setAuthorities(authorities);
+//            UserAuth userAuth=new UserAuth();
+//            userAuth.setUser(user);
+//            userAuth.setAuthorities(authorities);
+//            return userAuth;
+//        } else {
+//            throw  new UsernameNotFoundException("没有该用户");
+//        }
     }
 }
